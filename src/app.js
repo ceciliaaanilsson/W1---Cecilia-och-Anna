e53f88baa634be65e7a348edb7d20ba974545cbb
Cecilia Nilsson <cn223dw@student.lnu.se>
2024-09-09 11:35:11 +0200

    Last update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 
-const fileToRead = fs.readFileSync('./src/PlayingCard.js', 'utf-8')
+const fileToRead = fs.readFileSync('./src/app.js', 'utf-8')
 
 const lines = fileToRead.split('\n')
 
-function countLines (lines) {
+function countLines(lines) {
   let count = 0
 
   for (let i = 0; i < lines.length; i++) {
@@ -12,7 +12,35 @@ function countLines (lines) {
       count++
     }
   }
-  console.log(count)
+  console.log('Number of lines ' + count)
 }
 
 countLines(lines)
+
+function countInts(lines) {
+  let count = 0
+
+  for (let i = 0; i < lines.length; i++) {
+    let numbers = lines[i].split('').filter(char => !isNaN(char) && char !== ' ' && char !== '\r')
+    numbers.forEach(number => {
+      count++
+    });
+  }
+  console.log('Number of ints ' + count)
+}
+
+countInts(lines)
+
+function numberOfForLoops(lines) {
+  let count = 0
+
+  const word = 'for ('
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes(word)) {
+      count++
+    }
+  }
+  console.log('Number of forloops ' + count)
+}
+
+numberOfForLoops(lines)
